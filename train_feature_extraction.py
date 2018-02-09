3061d71a0d1b2e1ea548557a502edf346ce94501
Prabhat Telidevara <prabhat.tp@gmail.com>
2018-02-08 23:25:02 -0800

    added training and accuracy functions

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from alexnet import AlexNet
 import pandas as pd
+from sklearn.utils import shuffle
+
 
 # Load traffic signs data.
 with open("train.p", mode='rb') as f:
@@ -16,12 +18,14 @@ X, y = train['features'], train['labels']
 
 # Split data into Training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+num_examples = len(X_train)
 
 #print(np.shape(y_train), np.shape(y_test))
 
 # Define placeholder and resize operation.
 
 x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+keep_prob = tf.placeholder(tf.float32)
 resized = tf.image.resize_images(x, (227,227))
 
 # pass placeholder as first argument to `AlexNet`.
@@ -45,8 +49,52 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-# TODO: Define loss, training, accuracy operations.
-# HINT: Look back at your traffic signs project solution, you may
-# be able to reuse some the code.
+one_hot_y = tf.one_hot(y_train, len(X_train))
+rate = 0.001
+EPOCHS = 30
+BATCH_SIZE = 128
+
+# Define loss, training, accuracy operations.
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
 
 # TODO: Train and evaluate the feature extraction model.
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.75})
+
+        validation_accuracy = evaluate(X_test, y_test)
+        print("EPOCH {} ...".format(i + 1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+
+    saver.save(sess, './lenet')
+    print("Model saved")
