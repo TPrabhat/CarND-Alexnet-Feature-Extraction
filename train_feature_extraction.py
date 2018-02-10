00b72a7cf0824ad9635a0a0788e4ca33f5b9edd5
Prabhat Telidevara <prabhat.tp@gmail.com>
2018-02-10 10:19:16 -0800

    completed the architecture

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -20,16 +20,18 @@ X, y = train['features'], train['labels']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 num_examples = len(X_train)
 
-#print(np.shape(y_train), np.shape(y_test))
+#print(np.shape(X_train))
 
 # Define placeholder and resize operation.
 
 x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int64, None)
 keep_prob = tf.placeholder(tf.float32)
-resized = tf.image.resize_images(x, (227,227))
+x_resized = tf.image.resize_images(x, (227,227))
+
 
 # pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(x, feature_extract=True)
+fc7 = AlexNet(x_resized, feature_extract=True)
 # NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
@@ -38,11 +40,12 @@ fc7 = tf.stop_gradient(fc7)
 # Add the final layer for traffic sign classification.
 # determine shape of the second last layer of AlexNet
 
-shape=(fc7.get_shape().as_list()[-1], nb_classes)
-fc8_W = tf.Variable(tf.truncated_normal(shape, stddev = 0.1))
+shape = (fc7.get_shape().as_list()[-1], nb_classes)
+fc8_W = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 fc8_b = tf.Variable(tf.zeros(nb_classes))
 
 logits = tf.nn.xw_plus_b(fc7, fc8_W, fc8_b)
+
 probs = tf.nn.softmax(logits)
 
 init = tf.initialize_all_variables()
@@ -60,7 +63,7 @@ cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits
 loss_operation = tf.reduce_mean(cross_entropy)
 
 optimizer = tf.train.AdamOptimizer(learning_rate = rate)
-training_operation = optimizer.minimize(loss_operation)
+training_operation = optimizer.minimize(loss_operation, var_list=[fc8_W, fc8_b])
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
